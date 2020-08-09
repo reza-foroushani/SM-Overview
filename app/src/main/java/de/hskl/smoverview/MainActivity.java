cacd1c03b23db2cfd2f5ff19d6f3f5aeef17fb53
ahmad952 <ahmed-952@hotmail.com>
2020-08-09 15:38:28 +0200

    sffsdf

--- app/src/main/java/de/hskl/smoverview/MainActivity.java
+++ app/src/main/java/de/hskl/smoverview/MainActivity.java
@@ -1,8 +1,8 @@
 package de.hskl.smoverview;
 
-import androidx.appcompat.app.AppCompatActivity;
+        import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+        import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
