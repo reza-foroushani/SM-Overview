1848c69c8dce5d41a8b05aa4e3b166865110ea18
ahmad952 <ahmed-952@hotmail.com>
2020-08-19 15:41:49 +0200

    mani activity + mater activity

--- app/src/main/java/de/hskl/smoverview/MainActivity.java
+++ app/src/main/java/de/hskl/smoverview/MainActivity.java
@@ -2,17 +2,37 @@ package de.hskl.smoverview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private Button Bachelor;
+    private Button Master;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);//hallo
         setContentView(R.layout.activity_main);
-// neu
-        // Kommentar Ahmad
 
-        // aus Branch
+Bachelor=(Button) findViewById(R.id.BACHELOR);
+Master=(Button) findViewById(R.id.MASTER);
+Bachelor.setOnClickListener(this);
+Master.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view== Master){
+            Intent MeinMeister =new Intent(this,MasterActivity.class);
+        }
+        if (view==Bachelor){
+
+        }
+
     }
 }
---
+++ app/src/main/java/de/hskl/smoverview/MasterActivity.java
@@ -0,0 +1,13 @@
+package de.hskl.smoverview;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MasterActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_master);
+    }
+}
