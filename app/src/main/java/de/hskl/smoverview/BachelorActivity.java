e28febcdba00dffa74e0787fe7e451cd740befbe
Reza Foroushani <ghah0001@stud.hs-kl.de>
2020-08-21 11:38:06 +0200

    neue aktivity

---
+++ app/src/main/java/de/hskl/smoverview/BachelorActivity.java
@@ -0,0 +1,15 @@
+package de.hskl.smoverview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BachelorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bachelor);
+
+    }
+}
