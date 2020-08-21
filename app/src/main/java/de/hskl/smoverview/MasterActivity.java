946f3bfc8ebabfcc3c5b734cbb3d81e44cedba3d
ahmad952 <ahmed-952@hotmail.com>
2020-08-21 11:46:45 +0200

    context menue

--- app/src/main/java/de/hskl/smoverview/MasterActivity.java
+++ app/src/main/java/de/hskl/smoverview/MasterActivity.java
@@ -3,11 +3,15 @@ package de.hskl.smoverview;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 
@@ -22,24 +26,26 @@ public class MasterActivity extends AppCompatActivity implements View.OnClickLis
     ListView fachbereichliste ;
     ArrayList<String> FachbereichMasterItem = new ArrayList<>();
     ArrayAdapter<String> items;
-    //füe pluss floatingButton
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_master);
+        //füe pluss floatingButton
         FloatingActionButton myFab = (FloatingActionButton)  findViewById(R.id.fab);
         myFab.setOnClickListener(this);
        /* myFab.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Toast toast = Toast.makeText(getApplicationContext(), "add", Toast.LENGTH_SHORT);
                 toast.show();
-
             }
         });*/
-        fachbereichliste =(ListView) findViewById(R.id.FACHBEREICH_LISTE);
 
+        fachbereichliste =(ListView) findViewById(R.id.FACHBEREICH_LISTE);
         FachbereichMasterItem.add("Yellow");
+        // es wird listeview mit cotextmnue verbunden
+        registerForContextMenu(fachbereichliste);
 
 
 // arryadapter  in view-liste
@@ -79,5 +85,25 @@ public class MasterActivity extends AppCompatActivity implements View.OnClickLis
         super.onStart();
         fachbereichliste.setAdapter(items);
 
+    }
+//context menu
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        //die gibt objekt von class menuinfaleter
+        MenuInflater inflater= getMenuInflater();
+        // damit ich menue.xml zugreucfen , und zweite paramiter von cotext menu
+        inflater.inflate(R.menu.master_contextmenue,menu);
+    }
+//itemselected von cotextmenu
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId()==R.id.BEARBEITEN){
+
+        }
+        if(item.getItemId()== R.id.LOESCHEN){
+
+        }
+        return super.onContextItemSelected(item);
     }
 }
