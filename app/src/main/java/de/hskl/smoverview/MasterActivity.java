aa8acc14420728a4b11ac9fe5cdfb2885360390b
ahmad952 <ahmed-952@hotmail.com>
2020-08-21 23:52:04 +0200

    beirbeiten und löschen in contextmenue .bearbeiten  durch Dialog , was man bearbeitet hat , es wird in gleiche postion statt der allter text  gespeichert .  seve status von arraylist

--- app/src/main/java/de/hskl/smoverview/MasterActivity.java
+++ app/src/main/java/de/hskl/smoverview/MasterActivity.java
@@ -1,14 +1,20 @@
 package de.hskl.smoverview;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.ContextMenu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -19,13 +25,15 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 
-public class MasterActivity extends AppCompatActivity implements View.OnClickListener  {
+public class MasterActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemClickListener  {
     //requestCode
     public final int RequestCodHinzufuegen=11;
 
     ListView fachbereichliste ;
     ArrayList<String> FachbereichMasterItem = new ArrayList<>();
     ArrayAdapter<String> items;
+    String altetext;
+    int altertextposition;
 
 
     @Override
@@ -43,14 +51,18 @@ public class MasterActivity extends AppCompatActivity implements View.OnClickLis
         });*/
 
         fachbereichliste =(ListView) findViewById(R.id.FACHBEREICH_LISTE);
-        FachbereichMasterItem.add("Yellow");
+
         // es wird listeview mit cotextmnue verbunden
         registerForContextMenu(fachbereichliste);
 
 
-// arryadapter  in view-liste
+
+        // arryadapter  in view-liste
          items = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, FachbereichMasterItem);
         fachbereichliste.setAdapter(items);
+        //wenn ich auf Item von lister gedrükt, listern für items in viewliste
+        fachbereichliste.setOnItemClickListener(this);
+
     
     }
 
@@ -64,6 +76,7 @@ public class MasterActivity extends AppCompatActivity implements View.OnClickLis
         Toast toast = Toast.makeText(this, "add", Toast.LENGTH_SHORT);
         toast.show();
     }
+
     @Override
     // sobald die daten vin sub zurück ist, funktioniert onActivity direkt
     protected void onActivityResult(int requestCod ,int resultCode , Intent data){
@@ -85,25 +98,105 @@ public class MasterActivity extends AppCompatActivity implements View.OnClickLis
         super.onStart();
         fachbereichliste.setAdapter(items);
 
+    }// hier wenn ich auf item gedrüct ist zu activti von Eduard
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        Object o = fachbereichliste.getItemAtPosition(i);
+        //oder
+       // String selctItem = adapterView.getItemAtPosition(i).toString();
+        String pen = o.toString();
+        Toast toast = Toast.makeText(getApplicationContext(), pen , Toast.LENGTH_SHORT);
+        toast.show();
     }
+
 //context menu
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
         super.onCreateContextMenu(menu, v, menuInfo);
+        // durch  AdapterView.AdapterContextMenuInfo ich kann zielwiew (was ich lang gedrucht habe ),die ich erneut in das cast übertrage
+        AdapterView.AdapterContextMenuInfo info =
+                (AdapterView.AdapterContextMenuInfo) menuInfo;
+        altetext = ((TextView) info.targetView).getText().toString();
+        altertextposition=info.position;
+
+
         //die gibt objekt von class menuinfaleter
         MenuInflater inflater= getMenuInflater();
         // damit ich menue.xml zugreucfen , und zweite paramiter von cotext menu
         inflater.inflate(R.menu.master_contextmenue,menu);
     }
+
+
 //itemselected von cotextmenu
     @Override
     public boolean onContextItemSelected(@NonNull MenuItem item) {
         if(item.getItemId()==R.id.BEARBEITEN){
-
+         openDialog();
         }
         if(item.getItemId()== R.id.LOESCHEN){
 
         }
         return super.onContextItemSelected(item);
     }
+// bearbeiten Dialog
+     public void openDialog(){
+         AlertDialog.Builder dialogBearbeiten= new AlertDialog.Builder(this);
+         dialogBearbeiten.setTitle("Fachbereichname berbeiten ");
+         dialogBearbeiten.setMessage("Neu Name eingeben");
+         //bring die bearbeitlayout  und mit context zusammenlegen
+         final  View  bearbeitlayout =this.getLayoutInflater().inflate(R.layout.layout_dialogbearbeitenfachbereichvonmaster,null);
+         dialogBearbeiten.setView(bearbeitlayout);
+         final EditText newText=(EditText) bearbeitlayout.findViewById(R.id.BEARBEITEN_DIALOGBUTTON);
+         newText.setText(altetext);
+
+         dialogBearbeiten.setPositiveButton("speichern", new DialogInterface.OnClickListener() {
+
+
+             @Override
+             public void onClick(DialogInterface dialogInterface, int i) {
+                   //ich füge die neu Bearbeitung  in alter position hinzu und aktuallisieren
+                 FachbereichMasterItem.set(altertextposition,newText.getText().toString());
+                 items.notifyDataSetChanged();
+
+                  //TODO hier später mit datenbank
+                 Toast toast = Toast.makeText(getApplicationContext(), "geändert", Toast.LENGTH_SHORT);
+                 toast.show();
+
+             }
+         });
+         dialogBearbeiten.setNegativeButton("Abbrechen", new DialogInterface.OnClickListener() {
+             @Override
+             public void onClick(DialogInterface dialogInterface, int i) {
+
+             }
+         });
+         dialogBearbeiten.create();
+         dialogBearbeiten.show();
+
+     }
+     //save status 
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        ArrayList<String> title=new ArrayList<>();
+        for(int i=0;i<FachbereichMasterItem.size();i++){
+
+            title.add(FachbereichMasterItem.get(i));
+        }
+        outState.putStringArrayList("ARRYLIST",title);
+        Log.d("HSKL", "onSaveInstanceState() "+outState);
+    }
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        Log.d("HSKL", "onRestoreInstanceState() "+savedInstanceState);
+
+        for(int i=0;i<savedInstanceState.getStringArrayList("ARRYLIST").size();i++){
+
+            FachbereichMasterItem.add(savedInstanceState.getStringArrayList("ARRYLIST").get(i));
+        }
+    }
+
 }
