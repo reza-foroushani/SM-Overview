3036c025de0cc5bd41ce38292db6e1ed0b17956b
ahmad952 <ahmed-952@hotmail.com>
2020-08-23 16:02:28 +0200

    databank-muster mit alle  methoden die ich brauche add, gettAll , updet,delet, ich habe auch dafür  Master class , MasterAdbter mit layout (item-master). ,

--- app/src/main/java/de/hskl/smoverview/SubHinzufuegenFachbereichMasterActivity.java
+++ app/src/main/java/de/hskl/smoverview/SubHinzufuegenFachbereichMasterActivity.java
@@ -2,7 +2,6 @@ package de.hskl.smoverview;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,15 +12,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class SubHinzufuegenFachbereichMasterActivity extends AppCompatActivity implements View.OnClickListener {
     EditText konkreteEdettext ;
+    EditText konkreteEdettextBeschreibung ;
     Button speicherButton ;
     Button abbrechenButton ;
     String fachbereichName;
+    String fachbereichBeschreibung;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);//hallo
         setContentView(R.layout.activitz_subhinzufuegenfachbereichmaster);
 
         konkreteEdettext = (EditText) findViewById(R.id.FACHBEREICHNAME);
+        konkreteEdettextBeschreibung = (EditText) findViewById(R.id.BESCHREIBUNG_SUB);
         speicherButton = (Button) findViewById(R.id.SPEICHERN_BUTTON);
         abbrechenButton = (Button) findViewById(R.id.ABBRECHEN_BUTTON);
         //
@@ -35,9 +37,12 @@ public class SubHinzufuegenFachbereichMasterActivity extends AppCompatActivity i
     public void onClick(View view) {
         if (view== speicherButton) {
             fachbereichName = konkreteEdettext.getText().toString();
+            fachbereichBeschreibung = konkreteEdettextBeschreibung.getText().toString();
             Intent i = new Intent();
             //neu Intent ,damit ich ,was geschriben habe, wiede zurüch schiken
             i.putExtra("NAME", fachbereichName);
+            i.putExtra("BESCHREIBUNG", fachbereichBeschreibung);
+
             // schicke ich zurück
             setResult(Activity.RESULT_OK, i);
             finish();
