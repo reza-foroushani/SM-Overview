42983b24d8d137fdf5d3ad2fafc832de74fe0486
Reza Foroushani <ghah0001@stud.hs-kl.de>
2020-08-24 07:29:35 +0200

    Neue Liste

--- app/src/main/java/de/hskl/smoverview/BachelorIntent.java
+++ app/src/main/java/de/hskl/smoverview/BachelorIntent.java
@@ -6,14 +6,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.ActionMode;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
 
 public class BachelorIntent extends AppCompatActivity implements View.OnClickListener{
 
     TextView ausgabe;
     FloatingActionButton addButton;
+    ListView addListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +28,34 @@ public class BachelorIntent extends AppCompatActivity implements View.OnClickLis
         ausgabe = findViewById(R.id.TEXT_VIEW_BACHELOR);
         addButton = findViewById(R.id.ADD_BUTTEN);
 
+        addListView = (ListView)findViewById(R.id.ADD_LIST_VIEW);
+
+        ArrayList<String> arrayList = new ArrayList<>();
+
+        arrayList.add("Hallo");
+        arrayList.add("Hallo");
+        arrayList.add("Hallo");
+        arrayList.add("Hallo");
+        arrayList.add("Hallo");
+        arrayList.add("Hallo");
+        arrayList.add("Hallo");
+        arrayList.add("Hallo");
+        arrayList.add("Hallo");
+        arrayList.add("Hallo");
+        arrayList.add("Hallo");
+        arrayList.add("Hallo");
+        arrayList.add("Hallo");
+        arrayList.add("Hallo");
+        arrayList.add("Hallo");
+        arrayList.add("Hallo");
+        arrayList.add("Hallo");
+        arrayList.add("Hallo");
+
+
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this,android.R.layout.simple_expandable_list_item_1,arrayList);
+
+        addListView.setAdapter(arrayAdapter);
     }
 
     @Override
